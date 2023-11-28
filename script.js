e9df13f5f5988daf50ab6c2f06e876a42dda54fe
sakib531 <mdsakibahammad531@gmail.com>
2023-11-28 23:32:52 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".box-content", {
+    slidesPerView: 4,
+    // spaceBetween: 10,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 2,
+        },
+        520: {
+            slidesPerView: 3,
+        },
+        950: {
+            slidesPerView: 5,
+        },
+    },
+});
